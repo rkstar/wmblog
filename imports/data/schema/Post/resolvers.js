6be7951feef420ace06593914d9a60bd083bf959
David Fudge <rkstar@mac.com>
2017-09-29 10:40:28 -0400

    add posts schema skeleton

---
+++ imports/data/schema/Post/resolvers.js
@@ -0,0 +1,51 @@
+export const Query = { Post: {
+  async author(__, props, context) {
+
+  },
+
+  async tags(__, props, context) {
+
+  },
+
+  async likes(__, props, context) {
+
+  },
+
+  async comments(__, props, context) {
+
+  },
+
+  async getPost(__, props, context) {
+
+  },
+
+  async getPosts(__, props, context) {
+
+  },
+
+  async getComments(__, props, context) {
+
+  },
+}};
+
+export const Mutation = {
+  async addPost(__, props, context) {
+
+  },
+
+  async editPost(__, props, context) {
+
+  },
+
+  async deletePost(__, props, context) {
+
+  },
+
+  async boomarkPost(__, props, context) {
+
+  },
+
+  async unbookmarkPost(__, props, context) {
+
+  },
+};
