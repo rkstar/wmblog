5b286b86b1e29b7fc4d57671211f41f04f15d9ae
David Fudge <rkstar@mac.com>
2017-09-29 10:43:49 -0400

    add comment schema

---
+++ imports/data/schema/Comment/resolvers.js
@@ -0,0 +1,16 @@
+export const Query = { Comment: {
+}};
+
+export const Mutation = {
+  async addComment(__, props, context) {
+
+  },
+
+  async editComment(__, props, context) {
+
+  },
+
+  async deleteComment(__, props, context) {
+
+  },
+};
