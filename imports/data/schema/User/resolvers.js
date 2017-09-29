86c28568d8e4ce15e254b031a18de4b35e01786b
David Fudge <rkstar@mac.com>
2017-09-29 10:28:22 -0400

    user schema and resolvers skeleton

---
+++ imports/data/schema/User/resolvers.js
@@ -0,0 +1,31 @@
+export const Query = { User: {
+  async posts(__, props, context) {
+
+  },
+
+  async comments(__, props, context) {
+
+  },
+
+  async bookmarks(__, props, context) {
+
+  },
+
+  async getUsers(__, props, context) {
+
+  },
+
+  async getUser(__, props, context) {
+
+  },
+}};
+
+export const Mutation = {
+  async editUser(__, props, context) {
+
+  },
+
+  async deleteUser(__, props, context) {
+
+  },
+};
