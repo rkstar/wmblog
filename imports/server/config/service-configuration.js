1812f420524735f39e153a8a78f62c27b23f6bf9
David Fudge <rkstar@mac.com>
2017-09-29 14:52:06 -0400

    fix settings loop

--- imports/server/config/service-configuration.js
+++ imports/server/config/service-configuration.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { ServiceConfiguration } from 'meteor/service-configuration';
 
-Meteor.oauthServices.forEach(config => {
+Meteor.settings.oauthServices.forEach(config => {
   const { service, client } = config;
   ServiceConfiguration.configurations.update(
     { service },
