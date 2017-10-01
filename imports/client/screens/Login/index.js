39cb5f2a5b6ba7cf4f5d6c4d2bac9c90549f1dce
David Fudge <rkstar@mac.com>
2017-10-01 12:29:25 -0400

    add accounts-google implement login screen

--- imports/client/screens/Login/index.js
+++ imports/client/screens/Login/index.js
@@ -1,18 +1,54 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import Button from 'material-ui/Button';
-import style from './style.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Icons } from '../../theme/icons';
+import { Colors } from '../../theme/colors';
+import classes from './style.css';
 
-console.log('style:', style);
+const buttonProps = {
+  labelColor: Colors.app.white,
+  labelStyle: {
+    fontWeight: 700,
+  },
+  className: classes.button,
+};
+
+const labelStyle = {
+  fontWeight: 700,
+};
 
 export default () => (
-  <main className={style.layout}>
-    <img src="/img/weedmaps-logo.png" className={style.logo} alt="WeedMaps logo" />
-    <Button
-      primary
-      raised
-      label="Sign in with Twitter"
-      onClick={Meteor.loginWithTwitter}
-    />
+  <main className={classes.layout}>
+    <img src="/img/weedmaps-logo.png" className={classes.logo} alt="WeedMaps logo" />
+    <section>
+      <RaisedButton
+        {...buttonProps}
+        backgroundColor={Colors.social.twitter.blue}
+        label="Sign in with Twitter"
+        icon={Icons.drawFontIcon(Icons.twitter)}
+        onClick={Meteor.loginWithTwitter}
+      />
+      <RaisedButton
+        {...buttonProps}
+        backgroundColor={Colors.social.facebook.blue}
+        label="Sign in with Facebook"
+        icon={Icons.drawFontIcon(Icons.facebook)}
+        onClick={Meteor.loginWithFacebook}
+      />
+      <RaisedButton
+        {...buttonProps}
+        backgroundColor={Colors.social.github.green}
+        label="Sign in with Github"
+        icon={Icons.drawFontIcon(Icons.github)}
+        onClick={Meteor.loginWithGithub}
+      />
+      <RaisedButton
+        {...buttonProps}
+        backgroundColor={Colors.social.google.red}
+        label="Sign in with Google+"
+        icon={Icons.drawFontIcon(Icons.google)}
+        onClick={Meteor.loginWithGoogle}
+      />
+    </section>
   </main>
 );
