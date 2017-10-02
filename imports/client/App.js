12516c71c8f4c6a68cdbd89920db4e0527f88a0a
David Fudge <rkstar@mac.com>
2017-10-01 20:35:49 -0400

    get errors out of the console

--- imports/client/App.js
+++ imports/client/App.js
@@ -11,23 +11,23 @@ import Profile from './screens/Profile';
 import Login from './screens/Login';
 import ErrorScreen from './screens/Error';
 
+const AppRoutes = () => [
+  <Switch key="app-switcher">
+    <Route exact path="/" component={Feed} />
+    <AuthenticatedRoute exact path="/bookmarks" component={Bookmarks} />
+    <AuthenticatedRoute exact path="/likes" component={Likes} />
+    <AuthenticatedRoute path="/profile" component={Profile} />
+  </Switch>,
+  <BottomMenu key="bottom-menu" />,
+];
+
 export default () => (
   <BrowserRouter>
     <ScrollReset>
       <Switch>
         <Route path="/error" component={ErrorScreen} />
         <Route exact path="/login" component={Login} />
-
-        <main>
-          <Switch>
-            <Route exact path="/" component={Feed} />
-            <AuthenticatedRoute exact path="/bookmarks" component={Bookmarks} />
-            <AuthenticatedRoute exact path="/likes" component={Likes} />
-            <AuthenticatedRoute path="/profile" component={Profile} />
-          </Switch>
-          <BottomMenu />
-        </main>
-
+        <AppRoutes />
         <Redirect to="/error/404" />
       </Switch>
     </ScrollReset>
