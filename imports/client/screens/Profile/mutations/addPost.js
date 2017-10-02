f92b54130e1bf2c117936943ca19dd4bc76a3206
David Fudge <rkstar@mac.com>
2017-10-02 02:30:47 -0400

    prune unneeded code

--- imports/client/screens/Profile/mutations/addPost.js
+++ imports/client/screens/Profile/mutations/addPost.js
@@ -1,6 +1,5 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import update from 'react-addons-update';
 
 export default graphql(gql`
   mutation addPost($post: PostInput!) {
