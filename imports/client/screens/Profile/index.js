dbfa508de21899374c34a8a767f4bb91f131e795
David Fudge <rkstar@mac.com>
2017-10-01 21:35:06 -0400

    add post create fab

--- imports/client/screens/Profile/index.js
+++ imports/client/screens/Profile/index.js
@@ -1,12 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import TextField from 'material-ui/TextField';
 import Avatar from 'material-ui/Avatar';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 import { withTracker } from 'meteor/react-meteor-data';
 import { compose } from 'recompose';
 import editUserMutation from './mutations';
 import { Colors } from '/imports/client/theme/colors';
-
+import { Icons } from '/imports/client/theme/icons';
 import classes from './style.css';
 
 const backgroundImageUrl = 'http://lorempixel.com/960/200/abstract';
@@ -19,11 +21,15 @@ const floatingLabelStyle = {
   color: Colors.app.seafoam,
 };
 
-const Profile = ({ user, editUser }) => (
+const Profile = ({ user, editUser, history }) => (
   <section>
     <header className={classes.header} style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
       <h1>
-        <Avatar size={60} src={user.account.avatar} className={classes.avatar} />
+        <Avatar
+          size={60}
+          src={user.account.avatar}
+          className={classes.avatar}
+        />
         {user.profile.name}
       </h1>
     </header>
@@ -37,11 +43,19 @@ const Profile = ({ user, editUser }) => (
         })}
       />
     </main>
+    <FloatingActionButton
+      className={classes.fab}
+      backgroundColor={Colors.app.seafoam}
+      onClick={() => history.push('/profile/post/write')}
+    >
+      {Icons.drawFontIcon(Icons.edit)}
+    </FloatingActionButton>
   </section>
 );
 
 export default compose(
   editUserMutation,
+  withRouter,
   withTracker(() => ({
     user: { ...Meteor.user(), id: Meteor.userId() },
   })),
